5181802376a5b192014b0fd863ee1f338a8ec7c9
Bo Watts <bo@creativecomputer.ms>
2012-11-06 14:48:23 -0600

    Initial

---
+++ install.php
@@ -0,0 +1,5 @@
+<?php
+$YOUR_APP_ID = '';//supplied by facebook after registering app
+$YOUR_URL = '';
+?>
+<a href="http://www.facebook.com/dialog/pagetab?app_id=<?php echo $YOUR_APP_ID; ?>&next=<?php echo $YOUR_URL; ?>">Install App</a>
